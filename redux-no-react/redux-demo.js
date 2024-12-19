7ddf65cea88ad77d970dca513c0be74bbb1293e7
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-12-19 11:47:05 +0200

    Initial commit

---
+++ redux-no-react/redux-demo.js
@@ -0,0 +1,25 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === "increment") {
+        return {counter: state.counter + 1 }
+    } else if(action.type === "decrement"){
+        return {counter: state.counter - 1}
+     } else {
+        return { counter: state.counter }
+    }
+
+};
+
+const store = redux.createStore(counterReducer);
+
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+store.dispatch({ type: "increment" });
+store.dispatch({type: "decrement"});
+store.dispatch({type: "decrement"});
