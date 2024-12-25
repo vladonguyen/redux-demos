1b9120ce4a8cd8de0cb134549878975bf4ccbad0
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-12-26 01:54:51 +0200

    Adding counter value from redux store.

--- 01-starting-project/src/index.js
+++ 01-starting-project/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Provider} from "react-redux";
 import ReactDOM from 'react-dom/client';
+import store from './store';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /> </Provider>);
