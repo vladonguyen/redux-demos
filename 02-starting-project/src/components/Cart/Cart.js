099a0fcc524d30fc10225d0b58a222825361306e
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-12-30 01:45:33 +0200

    Store and ui slice created.

--- 02-starting-project/src/components/Cart/Cart.js
+++ 02-starting-project/src/components/Cart/Cart.js
@@ -1,17 +1,25 @@
+import { Fragment } from 'react';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
+import { useSelector } from 'react-redux';
+
+
 const Cart = (props) => {
+const cartIsVisible = useSelector(state=> state.ui.cartIsVisible);
+
   return (
-    <Card className={classes.cart}>
+    <Fragment>
+   {cartIsVisible && <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
         <CartItem
           item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
         />
       </ul>
-    </Card>
+    </Card>}
+    </Fragment>
   );
 };
 
